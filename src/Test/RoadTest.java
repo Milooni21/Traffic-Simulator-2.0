76843b21bcba37d548b5ace110f5d27bbb83e395
MILOONI YAGNIK <Milooni21>
2022-01-01 12:29:00 +0530

    Update RoadTest

--- src/Test/RoadTest.java
+++ src/Test/RoadTest.java
@@ -1,57 +1,60 @@
+package Test;
+
 import Model.Road;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 class RoadTest {
     Model.Road road = new Model.Road("0", 1, 5, new int[]{0, 0}, Road.Orientation.HORIZONTAL);
 
     @Test
     void getId() {
-        assertEquals(5, road.getLength());
+        Assertions.assertEquals(5, road.getLength());
     }
 
     @Test
     void getSpeedLimit() {
-        assertEquals(1, road.getSpeedLimit());
+        Assertions.assertEquals(1, road.getSpeedLimit());
     }
 
     @Test
     void getLength() {
-        assertEquals(5, road.getLength());
+        Assertions.assertEquals(5, road.getLength());
     }
 
     @Test
     void getStartLocationTest() {
         int[] expected1 = {0, 0};
         int[] actual = this.road.getStartLocation();
-        assertArrayEquals(expected1, actual);
+        Assertions.assertArrayEquals(expected1, actual);
     }
 
     @Test
     void getEndLocation() {
         int[] expected = {5, 0};
-        assertArrayEquals(expected, road.getEndLocation());
+        Assertions.assertArrayEquals(expected, road.getEndLocation());
     }
 
     @Test
     void getCars() {
         ArrayList<Model.Car> expected = new ArrayList<>();
-        assertEquals(expected, road.getVehiclesOnRoad());
+        Assertions.assertEquals(expected, road.getVehiclesOnRoad());
     }
 
     @Test
     void getLights() {
         ArrayList<Model.TrafficLight> expected = new ArrayList<>();
-        assertEquals(expected, road.getLightsOnRoad());
+        Assertions.assertEquals(expected, road.getLightsOnRoad());
     }
 
     @Test
     void getConnectedRoads() {
         ArrayList<Model.Road> expected = new ArrayList<>();
-        assertEquals(expected, road.getConnectedRoads());
+        Assertions.assertEquals(expected, road.getConnectedRoads());
     }
 }
+
+
+
